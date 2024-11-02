7859188d876e666621bdf11eee3cb24d387c6371
Chauxedgar <edchaux22A@udenar.edu.co>
2024-11-01 22:39:18 -0500

    3

---
+++ formulario/src/app/form-title/form-title.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-title',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-title.component.html',
+  styleUrl: './form-title.component.css'
+})
+export class FormTitleComponent {
+
+}
